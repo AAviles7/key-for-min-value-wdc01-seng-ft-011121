333d37f464a4a7b7812496a55a384d70b4db136f
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-01 22:11:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  
+  ans = String.new
+  temp = 0
+  name_hash.each do |key, value|
+    if temp > value
+      temp = value
+      ans = key
+    end
+  end
+  return ans
+  
 end
