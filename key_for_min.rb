58ebc0885e35f94f4c23149582ea85b38ad3d8fd
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-01 22:29:28 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,11 @@
 def key_for_min_value(name_hash)
   
   ans = nil
-  temp = name_hash.
+  
+  name_hash.each do |key, value|
+    temp = value
+  end
+  
   name_hash.each do |key, value|
     if temp < value
       temp = value
